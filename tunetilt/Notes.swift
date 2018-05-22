6c95796165e45144337b3730eeb2d96df0da786a
joelNguyenn1010 <nguyenngocanh590@gmail.com>
2018-05-22 16:26:24 +1000

    Firebase pod install and Persistent Data using Codable

---
+++ tunetilt/Notes.swift
@@ -0,0 +1,12 @@
+//
+//  Notes.swift
+//  tunetilt
+//
+//  Created by Nguyễn Ngọc Anh on 17/5/18.
+//  Copyright © 2018 Blinking Light Studios. All rights reserved.
+//
+
+import Foundation
+class Notes: Codable {
+    var notes: [String] = [""]
+}
--- tunetilt/controller/ViewController.swift
+++ tunetilt/controller/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
-
+    var db: Firestore!
+    var newNote = Notes()
+    var storage = SequencesStorage()
     override func viewDidLoad() {
         super.viewDidLoad()
+        readNotes()
         // Do any additional setup after loading the view, typically from a nib.
     }
+override func viewDidDisappear(_ animated: Bool) {
+    storage.loadData()
+    print("The sequences is \(self.newNote.notes)")
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func readNotes(){
+        db = Firestore.firestore()
+        db.collection("sequences").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    if let name = document.data()["notes1"] as? [String] {
+                        self.newNote.notes = name
+                        self.storage.saveData(theNote: self.newNote)
+                    }
+                }
+            }
+        }
+        
+    }
 
 }
 
